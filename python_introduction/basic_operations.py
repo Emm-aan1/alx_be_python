a165bc07efbea269e8ba27156ae900b47767fafb
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-06-19 08:44:02 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add_num = number1 + number2
+sub_num = number1 - number2
+mult_num = number1 * number2
+print(f"Addition of {number1} and {number2} is {add_num}")
+print(f"Subtraction of {number1} and {number2} is {sub_num}")
+print(f"Multiplication of {number1} and {number2} is {mult_num}")
