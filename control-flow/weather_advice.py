af870e75ca08ee2eee9173b71d0b96a0bb63e118
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-06-27 13:10:56 +0100

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,10 @@
 user_weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-match user_weather:
-  case 'sunny':
-    print('Wear a t-shirt and sunglasses.')
-  case 'rainy':
-    print("Don't forget your umbrella and a raincoat.")
-  case 'cold':
-    print('Make sure to wear a warm coat and a scarf.')
-  case _:
-    print("Sorry, I don't have recommendations for this weather.")
-
+if user_weather == 'sunny':
+  print("Wear a t-shirt and sunglasses.")
+elif user_weather == 'rainy':
+  print("Don't forget your umbrella and a raincoat.")
+elif user_weather == 'cold':
+  print("Make sure to wear a warm coat and a scarf.")
+else:
+  print("Sorry, I don't have recommendations for this weather.")
