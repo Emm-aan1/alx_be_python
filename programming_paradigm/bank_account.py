f818a5a3986a7db8557e07114eacd904f062d888
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-07-12 18:04:35 +0100

    Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        """Initializes a BankAccount object with an optional initial balance."""
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        """Adds the amount to the account balance."""
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        """Debits account if funds sufficient, denies withdrawal if otherwise."""
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            print("Insufficient funds.")
+            return False
+
+    def display_balance(self):
+        """Prints the account balance."""
+        print(f"Balance: ${self.account_balance}")
+
